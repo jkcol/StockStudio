20b075b5530458358f1157a7fba2dd1c38f42762
Jayden Kim <jaydenk2@illinois.edu>
2025-10-19 18:50:08 -0500

    Initial Commit

---
+++ StockStudio/StockStudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockStudioApp.swift
+//  StockStudio
+//
+//  Created by Jayden Kim on 10/19/25.
+//
+
+import SwiftUI
+
+@main
+struct StockStudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
